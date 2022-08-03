2b5e027ae6b3f99459ec72f0cf0c7f22cf190ff8
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-03 17:54:16 -0300

    Teste url dashboard

--- users/tests/test_urls.py
+++ users/tests/test_urls.py
@@ -3,13 +3,17 @@ from django.urls import reverse
 
 
 class UsersURLTest(TestCase):
-    def test_users_register_is_correct(self):
+    def test_users_register_url_is_correct(self):
         url = reverse('cadastro')
         self.assertEqual(url, '/user/cadastro/')
 
-    def test_users_login_is_correct(self):
+    def test_users_login_url_is_correct(self):
         url = reverse('login')
         self.assertEqual(url, '/user/login/')
+
+    def test_users_dashboard_url_is_correct(self):
+        url = reverse('dashboard')
+        self.assertEqual(url, '/user/dashboard/')
 
 """
 urlpatterns = [
