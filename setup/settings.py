24a9cc69b3432a5334055dbd15222b33ed33c993
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-04 16:29:38 -0300

    Adicionando messages tags ao settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
