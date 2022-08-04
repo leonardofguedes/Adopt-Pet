ecd97a23e27df2d0c2b13c9d0a088c555f2d27ae
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-04 09:42:30 -0300

    Desenvolvendo o método view para que receba as informações de post e impeça dados errados de serem salvos.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 
 def cadastro(request):
-    return render(request, 'users/pages/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            return redirect('cadastro')
+        if not email.strip():
+            return redirect('cadastro')
+        if senha!= senha2:
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        return redirect('login')
+    else:
+        return render(request, 'users/pages/cadastro.html')
 
 def login(request):
     return render(request, 'users/pages/login.html')
