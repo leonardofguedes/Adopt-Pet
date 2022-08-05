96cf1df57156c00153e50371cd5c09575ebd2602
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-05 16:59:44 -0300

    Adicionando photo detail para usá-los depois

--- dogncat/models.py
+++ dogncat/models.py
@@ -12,6 +12,9 @@ class Animal(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='animals/covers/%Y/%m/%d/', blank=True, default='')
+    photo_detail = models.ImageField(upload_to='animals/photo_detail/%Y/%m/%d/', blank=True, default='')
+    photo_detail_two = models.ImageField(upload_to='animals/photo_detail_two/%Y/%m/%d/', blank=True, default='')
+    photo_detail_three = models.ImageField(upload_to='animals/photo_detail_three/%Y/%m/%d/', blank=True, default='')
     type_of_animal = models.CharField(blank=False, null=False, default='U', max_length=25)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
