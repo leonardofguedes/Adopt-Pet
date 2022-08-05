75091d68479df9d13a9012c77cff24cde87a7c99
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-05 17:03:52 -0300

    Commit

--- dogncat/urls.py
+++ dogncat/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import home_view, by_animal_type_view, animal
+from .views import home_view, animal
 
 app_name = 'dogncat'
 
 urlpatterns = [
     path('', home_view, name="home"),
-    path('animal/<int:id>/', animal, name="animal"),
+    path('animal/<int:id>/', animal, name="animal")
     ]
 
