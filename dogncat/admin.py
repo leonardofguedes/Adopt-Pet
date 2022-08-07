e9811c4ef172f9b7947c416c310b034f5fd19450
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-07 14:11:30 -0300

    Commit

--- dogncat/admin.py
+++ dogncat/admin.py
@@ -1,21 +1,12 @@
 from django.contrib import admin
-from .models import Animal, Photo
+from .models import Animal
 
-
-class PhotoAdmin(admin.StackedInline):
-    model = Photo
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
-    inlines = [PhotoAdmin]
-
-    class Meta:
-        model = Animal
-
-
-@admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
     ...
+
+
 
 
 
