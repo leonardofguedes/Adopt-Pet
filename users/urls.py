8ac1ddc4261b1172efad882c1beaa9f84d74bd56
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-08 16:34:14 -0300

    Criando a url de delete

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('dashboard/', dashboard, name='dashboard'),
     path('logout/', logout, name='logout'),
-    path('newpet/', newpet, name='newpet')
+    path('newpet/', newpet, name='newpet'),
+    path('delete/<int:id>/', dashboard_delete, name='dashboard_delete'),
 ]
