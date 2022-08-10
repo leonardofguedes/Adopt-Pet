518b0ac4162c151d9d98fcbe22c3f235aefc9f33
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-10 13:35:12 -0300

    Refatorando a url com a nova class view criada

--- dogncat/urls.py
+++ dogncat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_view, animal
+from .views import HomeView, animal
 
 app_name = 'dogncat'
 
 urlpatterns = [
-    path('', home_view, name="home"),
+    #path('', home_view, name="home"),
+    path('', HomeView.as_view(), name="home"),
     path('animal/<int:id>/', animal, name="animal")
     ]
 
